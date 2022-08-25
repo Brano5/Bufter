6e76a48629ccce755039f604037c14efa4bd9aa4
Brano5 <97288158+Brano5@users.noreply.github.com>
2022-08-25 15:54:59 +0200

    small changes

---
+++ Bufter/Models/BuyLog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bufter.Models
+{
+    public class BuyLog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Room { get; set; }
+        public string Person { get; set; }
+        public string Item { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
