033eab2007c0bbe5c89032f5b088de13eaeef974
Branko Záchemský <bzachemsky@gmail.com>
2022-09-23 10:56:42 +0200

    drdrh

---
+++ Bufter/Controllers/ChartController.cs
@@ -0,0 +1,22 @@
+﻿using Bufter.Data;
+using Bufter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bufter.Controllers
+{
+    public class ChartController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public ChartController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View("chart", new Tuple<IEnumerable<BuyLog>, IEnumerable<Log>>(_db.BuyLog, _db.Log));
+        }
+    }
+}
