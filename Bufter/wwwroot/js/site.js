7ff7293f0ddafb5061058aa7b1d0070a353bb3b8
Branko Záchemský <bzachemsky@gmail.com>
2022-11-16 10:58:12 +0100

    sadfsdfdsf

--- Bufter/wwwroot/js/site.js
+++ Bufter/wwwroot/js/site.js
@@ -45,7 +45,7 @@ if (addMoneyModal != null) {
     })
 }
 
-//Popovers
+//Popovers from bootstrap
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
@@ -139,3 +139,24 @@ function chartPlotly(x, y) {
     //Display Plotly
     Plotly.newPlot("myPlot", data, layout);
 }
+
+//form validation from bootstrap
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
